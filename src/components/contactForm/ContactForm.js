f843c314725f847660a551be8ade8b2a91b12118
unknown <sreetejreddy1998@gmail.com>
2021-12-20 20:08:34 +0530

    fix phone

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -5,8 +5,8 @@ const ContactForm = () => {
 
   return (
     <div className="contact-form-wrapper text-contact">
-      <h2>Email: sreetejreddy1998@gmail.com</h2>
-      <h2>Phone: +91 7337470492</h2>
+      <h2>sreetejreddy1998@gmail.com</h2>
+      <h2>Phone: +91 7337470452</h2>
     </div>
   )
 }
