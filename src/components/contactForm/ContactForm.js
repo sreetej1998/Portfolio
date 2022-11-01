8183e72a9bec3b6071460792f30788c66c3fb6f2
unknown <sreetejreddy1998@gmail.com>
2022-11-01 12:39:57 -0700

    fix: update info

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -5,8 +5,9 @@ const ContactForm = () => {
 
   return (
     <div className="contact-form-wrapper text-contact">
-      <h2>sreetejreddy1998@gmail.com</h2>
-      <h2>Phone: +91 7337470452</h2>
+      <h2>Personal mail - sreetejreddy1998@gmail.com</h2>
+      <h2>UCI mail - snagarim@uci.edu</h2>
+      <h2>Phone: +1 959-659-9031</h2>
     </div>
   )
 }
